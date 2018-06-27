73b03315b06de8a375b5a3120a7adaac2472c82b
hongboz <hongboca@gmail.com>
2018-06-27 12:41:24 -0400

    initial commit

---
+++ desktop/src/com/somoplay/magicworld/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.somoplay.magicworld.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.somoplay.magicworld.MagicWorld;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MagicWorld(), config);
+	}
+}

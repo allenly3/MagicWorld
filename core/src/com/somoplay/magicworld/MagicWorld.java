da5710ef559c94349b1f382a45b85713681448a0
allenly3 <allenly3@163.com>
2018-06-28 14:52:12 -0400

    6.28 14:41

--- core/src/com/somoplay/magicworld/MagicWorld.java
+++ core/src/com/somoplay/magicworld/MagicWorld.java
@@ -3,31 +3,28 @@ package com.somoplay.magicworld;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.somoplay.magicworld.Resource.LoadResource;
+import com.somoplay.magicworld.Screens.MenuScreen;
 
 public class MagicWorld extends Game {
 
-	public  static float screenWidth, screenHeight;// 
+	public static float screenWidth,screenHeight;// get screen width and height from user device
+	public MenuScreen menuScreen;
 
 
-
-	@Override
 	public void create () {
 		screenWidth=Gdx.graphics.getWidth();
 		screenHeight=Gdx.graphics.getHeight();
-		//System.out.println(screenHeight+" "+screenWidth);
 
+		menuScreen=new MenuScreen(this);
+		setScreen(menuScreen);
 
 	}
 
-
-//	public void render () {
-//		Gdx.gl.glClearColor(1, 0, 0, 1);
-//		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-//
-//	}
 
 	@Override
 	public void dispose () {

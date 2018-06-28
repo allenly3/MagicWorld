d54f46cbeb8e1013cbc31818b4baa06df342c532
allenly3 <allenly3@163.com>
2018-06-28 10:46:00 -0400

    0628 10:45

--- core/src/com/somoplay/magicworld/MagicWorld.java
+++ core/src/com/somoplay/magicworld/MagicWorld.java
@@ -8,27 +8,29 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class MagicWorld extends Game {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
+
+	public  static float screenWidth, screenHeight;// 
+
+
 
 	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+	public void create () {
+		screenWidth=Gdx.graphics.getWidth();
+		screenHeight=Gdx.graphics.getHeight();
+		//System.out.println(screenHeight+" "+screenWidth);
+
+
 	}
+
+
+//	public void render () {
+//		Gdx.gl.glClearColor(1, 0, 0, 1);
+//		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+//
+//	}
 
 	@Override
 	public void dispose () {
-		batch.dispose();
-		img.dispose();
+
 	}
 }
--- core/src/com/somoplay/magicworld/Screens/MenuScreen.java
+++ core/src/com/somoplay/magicworld/Screens/MenuScreen.java
@@ -1,9 +1,37 @@
 package com.somoplay.magicworld.Screens;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.utils.viewport.Viewport;
+import com.somoplay.magicworld.MagicWorld;
+import com.somoplay.magicworld.Resource.LoadResource;
 
 public class MenuScreen implements Screen
 {
+    LoadResource loadResource;
+
+    Viewport viewport;
+    MagicWorld game;
+    Stage stage;
+    Body body;
+
+    public MenuScreen(MagicWorld game)
+    {
+        this.game=game;
+        init();
+    }
+
+    public void init()
+    {
+        loadResource=new LoadResource();
+
+
+
+    }
+
     public void show() {
 
     }
@@ -11,7 +39,12 @@ public class MenuScreen implements Screen
     @Override
     public void render(float delta) {
 
-    }
+            Gdx.gl.glClearColor(1, 0, 0, 1);
+            Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        }
+
+
 
     @Override
     public void resize(int width, int height) {
